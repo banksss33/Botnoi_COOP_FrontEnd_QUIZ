1eb63bc6cc07c8b77d9b05eec3951bfea031fad1
banksss33 <super.b99@hotmail.com>
2024-03-25 21:48:56 +0700

    update Q3

--- src/Q3.tsx
+++ src/Q3.tsx
@@ -1,4 +1,10 @@
 import "./Q3.css";
+import { ReactComponent as Icon1 } from "./icons/icon1.svg";
+import { ReactComponent as Icon2 } from "./icons/icon2.svg";
+import { ReactComponent as Icon3 } from "./icons/icon3.svg";
+import { ReactComponent as Icon4 } from "./icons/icon4.svg";
+import { ReactComponent as Icon5 } from "./icons/icon5.svg";
+import { ReactComponent as Icon6 } from "./icons/icon6.svg";
 
 function Q3() {
   return (
@@ -11,13 +17,17 @@ function Q3() {
 
         <div className="inner-grid">
           <div className="pd">
-
+            <div className="box">
+              Helo
+            </div>
           </div>
           <div className="ns">
 
           </div>
           <div className="dt">
-            
+            <div className="box2">
+
+            </div>
           </div>
         </div>
       </div>
